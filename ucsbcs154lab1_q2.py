89875913090fcc4fa581cc0cb62fb9323afd3d9d
Jakinda <qzhang423@ucsb.edu>
2026-01-13 23:37:42 -0800

    aaaa

---
+++ ucsbcs154lab1_q2.py
@@ -0,0 +1,18 @@
+# ucsbcs154lab1
+# All Rights Reserved
+# Copyright (c) 2023 Regents of the University of California
+# Distribution Prohibited
+
+import pyrtl
+# Simple example of 1-bit 2:1 mux
+
+val_a = pyrtl.Input(bitwidth=1, name='a')
+val_b = pyrtl.Input(bitwidth=1, name='b')
+select = pyrtl.Input(bitwidth=1, name='s')
+result = pyrtl.Output(bitwidth=1, name='o_wg')
+
+# Important: Assignments inside a "conditional_assignment"
+# are done with "|=" instead of the usual "<<="
+a_path = (~select) & val_a
+b_path = select & val_b
+result <<= a_path | b_path
